d34b5f3eaf320b9743fe2c63e37d929175f72fcf
yash069 <yash069@live.com>
2015-05-22 23:46:34 +0530

    Proper escaping
    Proper escaping in heredoc format to put a string into stdin (cat <<EOF .. EOF)

--- seedbox.sh
+++ seedbox.sh
@@ -104,20 +104,20 @@ install_rutorrent(){
 	svn -q co http://svn.rutorrent.org/svn/filemanager/trunk/filemanager/ /var/www/rutorrent/plugins/filemanager/
 	cat > /var/www/rutorrent/plugins/filemanager/conf.php <<END
 <?php
-$fm['tempdir'] = '/tmp'; // path were to store temporary data ; must be writable
-$fm['mkdperm'] = 755; // default permission to set to new created directories
+\$fm['tempdir'] = '/tmp'; // path were to store temporary data ; must be writable
+\$fm['mkdperm'] = 755; // default permission to set to new created directories
 // set with fullpath to binary or leave empty
-$pathToExternals['rar'] = '/usr/local/bin/rar';
-$pathToExternals['zip'] = '/usr/bin/zip';
-$pathToExternals['unzip'] = '/usr/bin/unzip';
-$pathToExternals['tar'] = '/bin/tar';
-$pathToExternals['gzip'] = '/bin/gzip';
-$pathToExternals['bzip2'] = '/bin/bzip2';
+\$pathToExternals['rar'] = '/usr/local/bin/rar';
+\$pathToExternals['zip'] = '/usr/bin/zip';
+\$pathToExternals['unzip'] = '/usr/bin/unzip';
+\$pathToExternals['tar'] = '/bin/tar';
+\$pathToExternals['gzip'] = '/bin/gzip';
+\$pathToExternals['bzip2'] = '/bin/bzip2';
 // archive mangling, see archiver man page before editing
-$fm['archive']['types'] = array('rar', 'zip', 'tar', 'gzip', 'bzip2');
-$fm['archive']['compress'][0] = range(0, 5);
-$fm['archive']['compress'][1] = array('-0', '-1', '-9');
-$fm['archive']['compress'][2] = $fm['archive']['compress'][3] = $fm['archive']['compress'][4] = array(0);
+\$fm['archive']['types'] = array('rar', 'zip', 'tar', 'gzip', 'bzip2');
+\$fm['archive']['compress'][0] = range(0, 5);
+\$fm['archive']['compress'][1] = array('-0', '-1', '-9');
+\$fm['archive']['compress'][2] = \$fm['archive']['compress'][3] = \$fm['archive']['compress'][4] = array(0);
 ?>
 END
 }
